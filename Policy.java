7772b26b32afc1ae23bfe57b21b054ad4cb9d043
Ezraela Hinton <ehinton0@lab.cpcc.edu>
2025-02-17 11:43:59 -0500

    Update Policy.java
    Added code for Policy class.

---
+++ Policy.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Policy{
+   private int policyNumber;
+   private String providerName;
+   private String firstName;
+   private String lastName;
+   private int age;
+   private String; smokingStatus;
+   private double height;
+   private double weight;
+   
+   public Policy() {
+      this(0, "Unknown", "Unknown", "Unknown", 0, "non-smoker", 0.0, 0.0);
+      }
+     
+      public Policy(int policyNumber, String providerName, String firstName, String lastName,
+                  int age, String smokingStatus, double height, double weight) {
+        this.policyNumber = policyNumber;
+        this.providerName = providerName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.smokingStatus = smokingStatus;
+        this.height = height;
+        this.weight = weight;
