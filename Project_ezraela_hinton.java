5573b076901982d658721eb2371dd471881b02db
ehinton0 <ehinton0@lab.cpcc.edu>
2025-03-24 09:33:41 -0400

    Update Project_ezraela_hinton.java
    Modified to read input from a file

--- Project_ezraela_hinton.java
+++ Project_ezraela_hinton.java
@@ -1,15 +1,30 @@
-public class Policy {
-    private int policyNumber;
-    private String providerName;
-    private Policyholder policyholder;
+import java.io.*;
+import java.util.*;
 
-    public Policy(int policyNumber, String providerName, Policyholder policyholder) {
-        this.policyNumber = policyNumber;
-        this.providerName = providerName;
-        this.policyholder = policyholder;
-    }
+public class Demo {
+    public static void main(String[] args) {
 
-    public int getPolicyNumber() {
-        return policyNumber;
+        try (Scanner fileScanner = new Scanner(new File("PolicyInformation.txt"))) {
+            while (fileScanner.next()) {
+                String providerName = fileScanner.next();
+                String firstName = fileScanner.next();
+                int age = fileScanner.nextInt();
+                double height = fileScanner.nextDouble();
+                double weight = fileScanner.nextDouble();
+                String lastName = fileScanner.next();
+                int policyNumber = fileScanner.nextInt();
+                String smokingStatus = fileScanner.next();
+               
+                Policyholder policyholder = new Policyholder(firstName, lastName, age, smokingStatus, height, weight);
+                policies.(new Policy(policyNumber, providerName, policyholder));
+            }
+      
+            System.out.println("Error: File not found.");
+        }
+
+     
+        for (Policy policy : policies) {
+            System.out.println(policy);
+        }
     }
 }
