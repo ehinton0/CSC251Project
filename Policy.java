e99d0958ffa54b9af513ee24a08924d671160821
ehinton0 <ehinton0@lab.cpcc.edu>
2025-03-24 09:30:14 -0400

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -1,39 +1,15 @@
-import java.util.Scanner;
-
-public class Policy{
-   private int policyNumber;
-   private String providerName;
-   private String firstName;
-   private String lastName;
-   private int age;
-   private String; smokingStatus;
-   private double height;
-   private double weight;
-   
-   public Policy() {
-      this(0, "Unknown", "Unknown", "Unknown", 0, "non-smoker", 0.0, 0.0);
-      }
-     
-      public Policy(int policyNumber, String providerName, String firstName, String lastName,
-                  int age, String smokingStatus, double height, double weight) {
+public class Policy {
+    private String providerName;
+    private Policyholder policyholder;
+    private int policyNumber;
+//@param
+    public Policy(int policyNumber, String providerName, Policyholder policyholder) {
         this.policyNumber = policyNumber;
-        this.providerName = providerName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.smokingStatus = smokingStatus;
-        this.height = height;
-
-        this.weight = weight;
-        
-        public Policy(int policyNumber, String providerName, String firstName, String lastName,
-                  int age, String smokingStatus, double height, double weight) {
-        this.policyNumber = policyNumber;
-        this.providerName = providerName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.smokingStatus = smokingStatus;
-        this.height = height;
->>>>>>> Stashed changes
-        this.weight = weight;
+        this.policyholder = policyholder;
+         this.providerName = providerName;
+    }
+//@return 
+    public int getPolicyNumber() {
+        return policyNumber;
+    }
+}
