d28f2a0a022fdf9dea8a3957a310ee3e781afdd3
ehinton0 <ehinton0@lab.cpcc.edu>
2025-04-14 10:31:55 -0400

    Policy.java
    Added code to implicitly call toString method and output information about Policy and PolicyHolders

--- Policy.java
+++ Policy.java
@@ -9,6 +9,20 @@ public class Policy {
          this.providerName = providerName;
     }
 //@return 
+    for (Policy p : policies) {
+            System.out.println(p);
+            System.out.println();
+
+            if (p.getPolicyHolder().getSmokingStatus().equalsIgnoreCase("smoker")) {
+                smokerCount++;
+            } else {
+                nonSmokerCount++;
+            }
+        }
+
+        System.out.println("Number of Policy objects created: " + Policy.getPolicyCount());
+        System.out.println("Number of Policyholders that are smokers: " + smokerCount);
+        System.out.println("Number of Policyholders that are non-smokers: " + nonSmokerCount);
     public int getPolicyNumber() {
         return policyNumber;
     }
